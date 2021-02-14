cf8c118b018f05308e23f324d81217c73f9d5efa
mshotskiy <sha10man3544@gmail.com>
2021-02-14 16:16:03 +0200

    Create project

---
+++ src/test/java/com/test/task/airsystem/AirSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.task.airsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
